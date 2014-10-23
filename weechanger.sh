cbf8c92c6a96645b85e3dd66b52fbf6421a233ad
Christophe Augello <christophe@augello.be>
2014-10-23 10:32:51 +0200

    init

---
+++ weechanger.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+#Set nick
+nickname="changeme"
+
+#Set weechat redhat profile name
+server_name="changeme"
+
+#Set Weechat FIFO
+fifo_api=`find $HOME/.weechat -name weechat_fifo_*`
+
+function change_nick(){
+    echo "irc.server.${server_name} */nick $1" > ${fifo_api}
+	echo "nick should be $1"
+	lastnick=$1
+	echo "lastnick is ${lastnick}"
+	}
+
+i=1
+dbus-monitor --session interface='org.freedesktop.Notifications',member='Notify'|
+while read -r line; do
+
+		if [[ ${line} =~ .*Talking.* ]]
+			then
+			   change_nick "${nickname}|call"
+
+		elif [[ ${line} =~ .*Idle.* ]] && [[ ${lastnick} != "${nickname}" ]]
+			then
+			   change_nick "${nickname}"
+		
+	    elif [[ ${line} == 'string "Current status is: Ready"' ]] && [[ ${lastnick} != ${nickname} ]]
+			then 
+				change_nick "${nickname}"
+		fi
+done
